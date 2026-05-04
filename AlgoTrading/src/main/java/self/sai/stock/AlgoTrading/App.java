bc2470cf3c4cc588458b7265a479fb7c89185a2a
Sai Kanimetta <skanimetta@opentext.com>
2026-05-04 08:02:05 +0530

    Safe commit

--- AlgoTrading/src/main/java/self/sai/stock/AlgoTrading/App.java
+++ AlgoTrading/src/main/java/self/sai/stock/AlgoTrading/App.java
@@ -3,11 +3,18 @@ package self.sai.stock.AlgoTrading;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
